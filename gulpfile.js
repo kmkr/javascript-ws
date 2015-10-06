ce2c20a8bc6f6a6edc72c7530384b2ec904050d0
Kris-Mikael Krister <krismikael@protonmail.ch>
2015-10-06 22:35:15 +0200

    Use colors also for single build

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('webpack', ['lint'], function(callback) {
     webpack(require('./webpack.config.js'), function(err, stats) {
         if(err) throw new gutil.PluginError('webpack', err);
         gutil.log('[webpack]', stats.toString({
+          colors: true
         }));
         callback();
     });
