cec3e57da37f50c4bd293be9b5d6fcfa0ae5ebc5
Kris-Mikael Krister <krismikael@protonmail.ch>
2015-10-06 20:40:10 +0200

    Use single quotes

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
           {
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: "babel-loader"
+            loader: 'babel-loader'
           }
         ]
       }
